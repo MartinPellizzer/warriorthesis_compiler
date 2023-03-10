ae2487f026e6579aadf730e3cdac70c5fa0919fb
Martin Pellizzer <martinpellizzer@gmail.com>
2023-03-10 18:13:44 +0100

    '

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ header = f'''
                 <a href="index.html">WarriorThesis</a>
                 <ul>
                     <li><a href="index.html">Home</a></li>
-                    <li><a href="free-ebook.html">FREE Ebook</a></li>
+                    <li><a href="workplace-bullying-survival-kit.html">Workplace Bullying Survival Kit</a></li>
                     <li><a href="about.html">About</a></li>
                 </ul>
             </nav>
